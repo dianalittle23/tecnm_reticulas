bf23fd6da45556d98e84ba6b139cf93348981ba2
dianalittle23 <149351372+dianalittle23@users.noreply.github.com>
2025-12-03 18:33:34 -0600

    vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   preview: {
-    allowedHosts: ["dependable-creation-production.up.railway.app"],
+  preview: {
+    host: "0.0.0.0",
+    port: 4173, // valor por defecto, Railway luego lo sobreescribe con $PORT
+    allowedHosts: [
+      "dependable-creation-production.up.railway.app",
+    ],
   },
-})
+});
